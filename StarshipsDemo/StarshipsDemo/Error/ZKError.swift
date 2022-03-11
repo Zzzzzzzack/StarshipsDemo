405fdcfac4d35a5710730912104e72ea0ff76d5a
Zack <zack@abacus.co>
2022-03-11 17:57:22 +1100

    Added networking error relate enums

---
+++ StarshipsDemo/StarshipsDemo/Error/ZKError.swift
@@ -0,0 +1,35 @@
+//
+//  ZKError.swift
+//  StarshipsDemo
+//
+//  Created by Zack on 11/3/22.
+//
+
+import Foundation
+
+/// A list of possible network related errors
+protocol ZKError: LocalizedError {
+
+}
+
+/// A list of possible network related errors
+enum ZKNetworkError {
+    case invalidURL
+    case serverError
+    case custom(message: String)
+}
+
+/// Implement the ZKError protocol
+extension ZKNetworkError: ZKError {
+    /// A localized message describing what error occurred.
+    var errorDescription: String? {
+        switch self {
+        case .invalidURL:
+            return "Invalid URL"
+        case .serverError:
+            return "Server Error"
+        case .custom(let message):
+            return message
+        }
+    }
+}
---
+++ StarshipsDemo/StarshipsDemoTests/ZKErrorTests.swift
@@ -0,0 +1,39 @@
+//
+//  ZKErrorTests.swift
+//  StarshipsDemoTests
+//
+//  Created by Zack on 11/3/22.
+//
+
+import XCTest
+
+class ZKErrorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testErrorDescriptions() throws {
+        let invalidURLError = ZKNetworkError.invalidURL
+        XCTAssertEqual(invalidURLError.errorDescription, "Invalid URL")
+
+        let serverError = ZKNetworkError.serverError
+        XCTAssertEqual(serverError.errorDescription, "Server Error")
+
+        let customMessage = "Custom Message Error"
+        let customError = ZKNetworkError.custom(message: customMessage)
+        XCTAssertEqual(customError.errorDescription, customMessage)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
