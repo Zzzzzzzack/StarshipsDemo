8fe79b584d955efbc36e10d0f98925c7d66e981d
Zack <zack@abacus.co>
2022-03-11 17:30:26 +1100

    Added starships related API enums

---
+++ StarshipsDemo/StarshipsDemo/Networking/ZKNetworkingAPI.swift
@@ -0,0 +1,85 @@
+//
+//  ZKNetworkingAPI.swift
+//  StarshipsDemo
+//
+//  Created by Zack on 11/3/22.
+//
+
+import Foundation
+
+private let domain = "https://swapi.dev/api"
+
+/// Methods of http request
+enum ZKHttpMethod: String {
+    case get, post
+}
+
+/// A type that can be used to get the API's url string and its related http method
+protocol ZKNetworkAPIProtocol {
+    // The full API url string, e.g. https://swapi.dev/api/starships/2",
+    var urlString: String { get }
+    
+    // The related http method of this API
+    var method: ZKHttpMethod { get }
+}
+   
+// A enum used to get the Network API info
+enum ZKNetworkAPI {
+    // Define the API categories
+    case starships(_ api: ZKNetworkAPI.Category.Starships)
+    
+    // The category used for grouped the APIs
+    // Use subclass here so it can only been used by this formate: ZKNetworkAPI.Category
+    class Category {}
+}
+
+// Implement the ZKNetworkAPIProtocol for all the APIs
+extension ZKNetworkAPI: ZKNetworkAPIProtocol {
+    var urlString: String {
+        switch self {
+        case .starships(let api):
+            return domain + "/" + api.categoryName + "/" + api.apiName
+        }
+    }
+    
+    var method: ZKHttpMethod {
+        switch self {
+        case .starships(let api):
+            return api.method
+        }
+    }
+}
+
+// Define the APIs under the starship category
+extension ZKNetworkAPI.Category {
+    enum Starships {
+        case list
+        case details(_ starshipID: String)
+    }
+}
+
+// Define the category names, API names and http methods for Starships APIs
+extension ZKNetworkAPI.Category.Starships {
+    // API category name
+    var categoryName: String {
+        return "starships"
+    }
+    
+    // API name
+    var apiName: String {
+        switch self {
+        case .list:
+            return ""
+        case .details(let starshipID):
+            return "\(starshipID)"
+        }
+    }
+    
+    // Related http method
+    var method: ZKHttpMethod {
+        switch self {
+        case .list, .details:
+            return .get
+        }
+    }
+}
---
+++ StarshipsDemo/StarshipsDemoTests/ZKNetworkingAPITest.swift
@@ -0,0 +1,37 @@
+//
+//  ZKNetworkingAPITest.swift
+//  StarshipsDemoTests
+//
+//  Created by Zack on 11/3/22.
+//
+
+import XCTest
+
+class ZKNetworkingAPITest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testStarshipsAPIEnums() throws {
+        let starshipsListAPI = ZKNetworkAPI.starships(.list)
+        XCTAssertEqual(starshipsListAPI.urlString, "https://swapi.dev/api/starships/")
+        XCTAssertEqual(starshipsListAPI.method, .get)
+        
+        let starshipDetailsAPI =  ZKNetworkAPI.starships(.details("2"))
+        XCTAssertEqual(starshipDetailsAPI.urlString, "https://swapi.dev/api/starships/2")
+        XCTAssertEqual(starshipDetailsAPI.method, .get)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
